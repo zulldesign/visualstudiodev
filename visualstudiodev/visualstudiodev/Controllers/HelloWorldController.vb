e73b70101d68d6e3b0899bdde3e01a09bd8973c7
zulldesign <admin@zulldesign.ml>
2014-09-15 12:21:21 +0800

    l

--- visualstudiodev/visualstudiodev/Controllers/HelloWorldController.vb
+++ visualstudiodev/visualstudiodev/Controllers/HelloWorldController.vb
@@ -9,8 +9,9 @@
             Return "This is my default action..."
         End Function
 
-        Public Function Welcome() As String
-            Return "This is the Welcome action method..."
+        Public Function Welcome(ByVal name As String, Optional ByVal numTimes As Integer = 1) As String
+            Dim message As String = "Hello " & name & ", NumTimes is: " & numTimes
+            Return "" & Server.HtmlEncode(message) & ""
         End Function
 
     End Class

afd79a4967f310a7b326f8434beaa57a5212060c
zulldesign <admin@zulldesign.ml>
2014-09-15 13:07:34 +0800

    l

--- visualstudiodev/visualstudiodev/Controllers/HelloWorldController.vb
+++ visualstudiodev/visualstudiodev/Controllers/HelloWorldController.vb
@@ -9,9 +9,10 @@
             Return View()
         End Function
 
-        Public Function Welcome(ByVal name As String, Optional ByVal numTimes As Integer = 1) As String
-            Dim message As String = "Hello " & name & ", NumTimes is: " & numTimes
-            Return "" & Server.HtmlEncode(message) & ""
+        Public Function Welcome(ByVal name As String, Optional ByVal numTimes As Integer = 1) As ActionResult
+            ViewBag.Message = "Hello Facebook " & name
+            ViewBag.NumTimes = numTimes
+            Return View()
         End Function
 
     End Class

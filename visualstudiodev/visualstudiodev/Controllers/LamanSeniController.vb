12997e0461a917a210d58eeb54d2b04ffee3d033
zulldesign <admin@zulldesign.ml>
2014-09-16 10:10:42 +0800

    l

---
+++ visualstudiodev/visualstudiodev/Controllers/LamanSeniController.vb
@@ -0,0 +1,17 @@
+﻿Namespace visualstudiodev
+    Public Class LamanSeniController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /LamanSeni
+
+        Public Function Index() As ActionResult
+            Return View()
+        End Function
+
+        Public Function Guitar(ByVal template As String, Optional ByVal download As Integer = 1) As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace

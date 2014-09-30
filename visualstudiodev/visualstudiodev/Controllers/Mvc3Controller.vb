e546e009b0ff23cff7d1c25f1e4fc0e2ff80afd1
zulldesign <admin@zulldesign.ml>
2014-09-30 08:45:14 +0800

    l

---
+++ visualstudiodev/visualstudiodev/Controllers/Mvc3Controller.vb
@@ -0,0 +1,13 @@
+﻿Namespace visualstudiodev
+    Public Class Mvc3Controller
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /Mvc3
+
+        Public Function Index() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace

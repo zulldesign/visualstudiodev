1162e8cbdf08e629daa2a21ddd6e51939531fb62
zulldesign <admin@zulldesign.ml>
2014-10-03 09:01:03 +0800

    l

---
+++ visualstudiodev/fb/Controllers/SyntaxhighlighterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fb.Controllers
+{
+    public class SyntaxhighlighterController : Controller
+    {
+        //
+        // GET: /Syntaxhighlighter/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

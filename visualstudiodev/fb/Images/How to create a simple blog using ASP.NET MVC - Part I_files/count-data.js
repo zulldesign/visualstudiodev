65ad7e1bedb5e3121d5160477e4ff6f94c8eb78a
zulldesign <admin@zulldesign.ml>
2014-10-08 10:37:28 +0800

    l

---
+++ visualstudiodev/fb/Images/How to create a simple blog using ASP.NET MVC - Part I_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"No Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/prideparrot.com\/blog\/archive\/2012\/12\/how_to_create_a_simple_blog_part1","comments":158}]});
+}
